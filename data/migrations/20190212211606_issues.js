65d82b97478cbc377c466346d6d9a7fc7ddd53b7
cocoitali <cmburatto@gmail.com>
2019-02-12 21:36:55 -0700

    MVP : changed comments data type

--- data/migrations/20190212211606_issues.js
+++ data/migrations/20190212211606_issues.js
@@ -1,25 +1,22 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('issues', tbl => {
-      tbl.increments('id')
-      tbl.string('issue_name').notNullable()
-    tbl.boolean('is_complete').defaultTo(false)
-    tbl.boolean('is_scheduled').defaultTo(false)
-    tbl.boolean('is_ignored').defaultTo(false)
-    tbl.string('comments').notNullable()
-    tbl.integer('school_id').unsigned()
+	return knex.schema.createTable('issues', tbl => {
+		tbl.increments('id')
+		tbl.string('issue_name').notNullable()
+		tbl.boolean('is_complete').defaultTo(false)
+		tbl.boolean('is_scheduled').defaultTo(false)
+		tbl.boolean('is_ignored').defaultTo(false)
+		tbl.text('comments').notNullable()
+		tbl.integer('school_id').unsigned()
 		tbl
 			.foreign('school_id')
 			.references('id')
-            .on('schools')
-    tbl.integer('user_id').unsigned()
-            tbl
-                .foreign('user_id')
-                .references('id')
-                .on('users')
-  })
-};
+			.on('schools')
+		tbl.integer('user_id').unsigned()
+		tbl
+			.foreign('user_id')
+			.references('id')
+			.on('users')
+	})
+}
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise) {}
