07840a0942fd99a56855eb8aaf2c629a3c9b9b74
cocoitali <cmburatto@gmail.com>
2019-02-12 20:33:14 -0700

    MVP : server running on 3000

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+const mwConfig = require('./data/mwConfig')
+const db = require('./data/dbConfig.js')
+
+const PORT = 3000
+const server = express()
+server.use(express.json())
+
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
+})
