c493c2fc4b441f3d161f3acd356e5a69dfa4da1b
cocoitali <cmburatto@gmail.com>
2019-02-16 09:34:13 -0700

    FIX : push misc changes

---
+++ react/login/src/App.js
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react'
+// // import { Route, NavLink } from 'react-router-dom'
+// import Login from './Login'
+
+// //STILL NEEDS LOTS OF WORK, AFTER I ADDED ROUTERS, I CAN NO LONGER GET A JWT??????
+// class App extends Component {
+// 	render() {
+// 		return (
+//             <Login />
+// 			<>
+// 				<header>
+// 					<nav>
+// 						<NavLink to='/api/login'>LogIn</NavLink>
+// 					</nav>
+// 				</header>
+// 				<main>
+// 					<Route path='/api/login' component={Login} />
+// 				</main>
+// 			</>
+// 		)
+// 	}
+// }
+
+// export default App
--- react/login/src/index.js
+++ react/login/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './Login';
+import Login from '../src/Login';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 
