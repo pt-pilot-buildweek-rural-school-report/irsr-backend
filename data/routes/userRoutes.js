563c7f349e13a2b01f1ce0bca57aa90c08a09e60
Ryan D Clausen <41310846+theryanclausen@users.noreply.github.com>
2019-02-21 19:02:57 -0700

    Revert "Revert "join for issues complete""

--- data/routes/userRoutes.js
+++ data/routes/userRoutes.js
@@ -3,9 +3,9 @@ const express = require('express')
 const db = require('../dbConfig')
 const router = express.Router()
 
-const { authenticate } = require('../auth/authenticate')
+const { authenticate, checkRole } = require('../auth/authenticate')
 
-router.get('/api/users', authenticate, (req, res) => {
+router.get('/', authenticate, (req, res) => {
 	db('users')
 		.select('id', 'username')
 		.then(users => {
@@ -16,7 +16,7 @@ router.get('/api/users', authenticate, (req, res) => {
 		})
 })
 
-router.get('/api/users/:id', (req, res) => {
+router.get('/:id', (req, res) => {
 	const { id } = req.params
 	db('users')
 		.where({ id })
@@ -30,7 +30,7 @@ router.get('/api/users/:id', (req, res) => {
 		})
 })
 
-router.put('/api/users/:id', (req, res) => {
+router.put('/:id', (req, res) => {
 	const { id } = req.params
 	const user = req.body
 	db('users')
@@ -46,7 +46,7 @@ router.put('/api/users/:id', (req, res) => {
 		})
 })
 
-router.delete('/api/users/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 	const { id } = req.params
 	db('users')
 		.where({ id })
